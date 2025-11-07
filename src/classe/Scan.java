6904b4c7df6c94e2735dbf5caece45291e37e430
elyance <rahantamalala23@gmail.com>
2025-11-07 15:56:39 +0300

    debug de Scan.java Teste Finalisation

--- src/classe/Scan.java
+++ src/classe/Scan.java
@@ -19,59 +19,40 @@ public class Scan {
     public List<Class<?>> getClassesAnnotatedWith() throws IOException {
         List<Class<?>> classes = new ArrayList<>();
 
-        List<Package> myPackages = new ArrayList<>();
-        Package[] listPackages = Package.getPackages();
-
-        for (Package package1 : listPackages) {
-            if (!package1.getName().startsWith("java") && !package1.getName().startsWith("sun") && !package1.getName().startsWith("jdk")) {
-                // System.out.println(package1.getName());
-                // classes.add(package1.getClass());
-                myPackages.add(package1);
-            } 
-        } 
-        // Pour chaque package, on doit récupérer les classes
-        for (Package p : myPackages) {
-            try {
-                List<Class<?>> classesInPackage = getClasses(p);
-                for (Class<?> cls : classesInPackage) {
-                    if (cls.isAnnotationPresent((Class<? extends Annotation>) annotation)) {
-                        classes.add(cls);
-                    }
-                }
-            } catch (ClassNotFoundException e) {
-                // Classe non trouvée dans ce package, ignorer
-            }
-        }
-        return classes;
-    }
-
-    public List<Class<?>> getClasses(Package p) throws IOException, ClassNotFoundException {
-        List<Class<?>> classes = new ArrayList<>();
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        String path = p.getName().replace('.', '/');
+        Enumeration<URL> resources = classLoader.getResources("");
 
-        // Récupérer les fichiers .class dans le répertoire correspondant
-        Enumeration<URL> resources = classLoader.getResources(path);
         while (resources.hasMoreElements()) {
             URL resource = resources.nextElement();
             File directory = new File(resource.getFile());
-            if (directory.exists()) {
-                File[] files = directory.listFiles();
-                if (files != null) {
-                    for (File file : files) {
-                        if (file.getName().endsWith(".class")) {
-                            String className = p.getName() + "." + file.getName().replace(".class", "");
-                            try {
-                                Class<?> cls = Class.forName(className);
-                                classes.add(cls);
-                            } catch (ClassNotFoundException e) {
-                                // Classe non trouvée, ignorer
-                            }
-                        }
+            findClasses(directory, "", classes);
+        }
+        // Pour chaque package, on doit récupérer les classes
+        return classes;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void findClasses(File directory, String packageName, List<Class<?>> classes) {
+        if (!directory.exists()) return;
+
+        File[] files = directory.listFiles();
+        if (files == null) return;
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                findClasses(file, packageName + file.getName() + ".", classes);
+            } else if (file.getName().endsWith(".class")) {
+                String className = packageName + file.getName().replace(".class", "");
+                try {
+                    Class<?> cls = Class.forName(className);
+                    if (cls.isAnnotationPresent((Class<? extends Annotation>) annotation)) {
+                        classes.add(cls);
                     }
+                } catch (Throwable e) {
+                    // Ignorer les erreurs de chargement
                 }
             }
         }
-        return classes;
     }
+
 }
